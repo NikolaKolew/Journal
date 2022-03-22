3879a8d500d298e48835d47b30d399d70d80f530
NikolaKolew <kolev518@gmail.com>
2022-03-22 10:13:52 +0200

    Update test_user_forms.py

--- Journal/auth_accounts/tests/forms/test_user_forms.py
+++ Journal/auth_accounts/tests/forms/test_user_forms.py
@@ -12,7 +12,7 @@ class ProfileFormsTests(TestCase):
         'password2': 'NikolaKU99881',
     }
 
-    IVALID_USER_DATA_COINTAINS_DIGITS_IN_NAMES = {
+    INVALID_USER_DATA_CONTAINS_DIGITS_IN_NAMES = {
         'first_name': '11Nikola',
         'last_name': 'Kolev22',
         'email': 'kolev777@gmail.com',
@@ -20,20 +20,19 @@ class ProfileFormsTests(TestCase):
         'password2': 'NikolaKU99881',
     }
 
-
-    def test_profile_register_form_valid_data_except_to_be_valid(self):
+    def test_profile_register_form_valid_data_expects_to_be_valid(self):
         data = self.VALID_USER_DATA
 
         form = UserRegistrationForm(data)
         self.assertTrue(form.is_valid())
 
-    def test_profile_register_form_invalid_data_except_to_be_invalid(self):
-        data = self.IVALID_USER_DATA_COINTAINS_DIGITS_IN_NAMES
+    def test_profile_register_form_invalid_data_expects_to_be_invalid(self):
+        data = self.INVALID_USER_DATA_CONTAINS_DIGITS_IN_NAMES
         form = UserRegistrationForm(data)
 
         self.assertFalse(form.is_valid())
 
-    def test_profile_edit_form_when_form_is_valid_except_to_be_valid(self):
+    def test_profile_edit_form_when_form_is_valid_expects_to_be_valid(self):
         data = {
             'first_name': self.VALID_USER_DATA['first_name'],
             'last_name': self.VALID_USER_DATA['last_name'],
@@ -43,7 +42,7 @@ class ProfileFormsTests(TestCase):
 
         self.assertTrue(form.is_valid())
 
-    def test_profile_edit_form_when_name_is_less_or_equal_to_one_excepts_validation_error(self):
+    def test_profile_edit_form_when_name_is_less_or_equal_to_one_expects_validation_error(self):
         data = {
             'first_name': 'N',
             'last_name': self.VALID_USER_DATA['last_name'],
@@ -53,9 +52,9 @@ class ProfileFormsTests(TestCase):
 
         self.assertFalse(form.is_valid())
 
-    def test_profile_edit_form_when_form_is_not_valid_except_to_be__not_valid(self):
+    def test_profile_edit_form_when_form_is_not_valid_expects_to_be__not_valid(self):
         data = {
-            'first_name': self.IVALID_USER_DATA_COINTAINS_DIGITS_IN_NAMES['first_name'],
+            'first_name': self.INVALID_USER_DATA_CONTAINS_DIGITS_IN_NAMES['first_name'],
             'last_name': self.VALID_USER_DATA['last_name'],
         }
 

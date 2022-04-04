4be183dd91281db89443b3dab27c8c47f09b246a
NikolaKolew <kolev518@gmail.com>
2022-04-04 16:04:35 +0300

    Register page img fixed

--- Journal/settings.py
+++ Journal/settings.py
@@ -122,7 +122,18 @@ else:
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = []
+AUTH_PASSWORD_VALIDATORS = [{
+            'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        },]
 
 if APP_ENVIRONMENT == 'Production':
     AUTH_PASSWORD_VALIDATORS = [
